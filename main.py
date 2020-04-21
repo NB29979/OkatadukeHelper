7d60a6ba2199799ca39bd6e5e6de4f8a6c5f103d
NB29979 <12462512+NB29979@users.noreply.github.com>
2020-04-21 23:25:20 +0900

    音声入力された日本語音声をコマンドに変換できるように

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import paho.mqtt.client as mqtt
 import re
 import json
+import string
 
 from regex_dict import  RegexDict
 
@@ -24,13 +25,25 @@ def on_message(_client, _userdata, _message):
     print('Received result: '+recog_result_)
 
     try:
-        pass
+        command = convert_recog_result_to_command(recog_result_)
+        print(command)
     except KeyError:
         print('Key error')
 
 
-def convert_recog_result_to_command():
-    pass
+def extract_variables(_recog_result):
+    words = [word for word in re.findall(r'([一-龥]*[ァ-ー]+|[一-龥]+)', _recog_result) if word != '']
+    var_dict_ = {}
+    if words:
+        for i in range(len(words)):
+            var_dict_['w'+str(i+1)] = words[i]
+    return var_dict_
+
+
+def convert_recog_result_to_command(_recog_result):
+    vars_dict_ = extract_variables(_recog_result)
+    selected_command_ = string.Template(okataduke_command_dict[_recog_result])
+    return selected_command_.safe_substitute(vars_dict_)
 
 
 if __name__ == '__main__':
